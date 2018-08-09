d1493f5fbab93c8d30ef513d439c9d82bc64f7ba
Admin <admin@Admins-MacBook-Pro.local>
2018-08-08 21:29:07 -0700

    Uploading my stack to-do onto github, helps me out with projects, feel free to use.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,146 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import $ from 'jquery';
+import { EWOULDBLOCK } from 'constants';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: [],
+      val : 'hello',
+      finished: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetch(); 
+    this.fetch2();
+  }
+
+  fetch() {
+    $.get('/1', (dat) => {
+      dat.forEach((el) => {
+        var arr = this.state.rows;
+        arr.unshift(<h2 id='item'>{el.value}</h2>);
+        this.setState({
+          rows: arr
+        });
+      });
+    });
+  }
+
+  fetch2() {
+    $.get('/2', (dat) => {
+      dat.forEach((el) => {
+        var arr = this.state.finished;
+        arr.unshift(<h2 id='item'>{el.value}</h2>);
+        this.setState({
+          finished: arr
+        });
+      });
+    });
+  }
+
+  add(event) {
+    event.preventDefault();
+    var arr = this.state.rows;
+    var saver = this.state.val;
+    arr.unshift(<h2 id='item'>{this.state.val}</h2>);
+    this.setState({
+      rows: arr,
+      val: ''
+    })
+    
+    $.post('/1', {'Content-Type': 'application/json', data: saver }, (data) => {
+      console.log('succ')
+    });
+  }
+
+  texter(event) {
+    var item = event.target.value;
+    event.preventDefault();
+    this.setState({
+      val: item
+    })
+  }
+
+  remove(event) {
+    event.preventDefault();
+    var arr = this.state.rows;
+    var removed = arr.shift();
+    var arr2 = this.state.finished;
+    arr2.unshift(removed);
+
+    this.setState({
+      rows: arr,
+      finished: arr2
+    });
+
+    $.post('/2', {'Content-Type': 'application/json', data: removed.props.children }, (data) => {
+      console.log('succ')
+    });
+  }
+
+  add2(event) {
+    event.preventDefault();
+    var arr = this.state.rows;
+    arr.push(<h2 id='item'>{this.state.val}</h2>);
+    this.setState({
+      rows: arr,
+      val: ''
+    });
+
+    $.post('/1', {'Content-Type': 'application/json', data: saver }, (data) => {
+      console.log('succ')
+    });
+
+
+
+  }
+
+  clear(event) {
+    event.preventDefault();
+    var arr = [];
+    this.setState({
+      rows: arr,
+      finished: []
+    });
+
+    $.ajax({
+      url: '/1',
+      type: 'DELETE'
+    });
+
+  }
+
+
+
   
   render() {
     return (
-      <div>
-        Hello React
+      <div id='main'>
+        <img src='https://s3.us-east-2.amazonaws.com/stacktodo/stackTodo.png' />
+        <div id='todo'>
+          {this.state.rows}
+        </div>
+        <div id='theforms'>
+        <form>
+          <input type='text' value ={this.state.val} onChange={this.texter.bind(this)} id='inn'/>
+          <input type='submit' onClick={this.add.bind(this)}  id='sub'/>
+          <input type='submit' value ='Enqueue' onClick={this.add2.bind(this)} id='sub' />
+        </form>
+        <div>
+          <button onClick={this.remove.bind(this)} id='fin'> Finished </button>
+          <button onClick={this.clear.bind(this)}> Clear </button>
+        </div>
+        </div>
+        <div id='todo'>
+          {this.state.finished}
+        </div>
       </div>
-    );
+    
+    )
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,93 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+/// mongoose stuff
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://sneej:123456a@ds051903.mlab.com:51903/stacktodo';
+mongoose.connect(mongoDB, { useNewUrlParser: true } , () => {
+  console.log('dayum')
+});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+var Schema = mongoose.Schema
+
+var todoSchema = new Schema({
+  value: String,
+  id: Number
+});
+
+var doneSchema = new Schema({
+  value: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var Final = mongoose.model('Final', doneSchema);
+
+
+app.post('/:n', (req, res) => {
+  var numero = req.params.n;
+
+  if (numero === '1') {
+  Todo.create({
+    value: req.body.data
+  });
+  } else if (numero === '2') {
+    console.log('yah')
+    var text = req.body.data
+    Final.create({
+      value: req.body.data
+    }).then(() => {
+      Todo.findOneAndRemove({'value': `${text}`}, (request, response) => {
+        console.log(request);
+        console.log(response)
+      });
+    });
+    }
+});
+
+app.get('/:n', (req, res) => {
+  var numero = req.params.n;
+  if (numero === '1') {
+    Todo.find((err, todos) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(todos);
+      }
+    });
+  } else if (numero === '2') {
+      Final.find((err, final) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(final);
+        }
+      });
+    }     
+  });
+
+app.delete('/:n', (req, res) => {
+  if (req.params.n === '1') {
+    Final.remove({}, () => {});
+    Todo.remove({}, () => {});  
+  }
+});
 
 app.use((req, res, next) => {
   console.log(`serving request ${req.method} at ${req.url}`);
   next();
 });
 
-app.use(express.static(path.join(__dirname, '../client/dist/')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
